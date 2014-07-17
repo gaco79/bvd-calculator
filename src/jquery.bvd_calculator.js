798df3b85f228899bc9435404ee53ad76a4ea3d9
Gareth Cooper <gareth@garethcooper.com>
2014-07-17 19:08:04 +0100

    Initial work on Rx validation

--- src/jquery.bvd_calculator.js
+++ src/jquery.bvd_calculator.js
@@ -19,43 +19,42 @@ window.console.log('Start BVD Calculator');
 			cylInput = $.fn.bvd_calculator.inputs.cyl = $('#cyl', this);
 			axisInput = $.fn.bvd_calculator.inputs.axis = $('#axis', this);
 
-			//Bind sph and cyl to power number formatter
+			// Bind sph and cyl to power number formatter
 			sphereInput.on("focusout", formatPower);
 			cylInput.on("focusout", formatPower);
-			
+
 			// Bind axis input to formatter
-			axisInput.on("focusout", formatAxis );
-			
+			axisInput.on("focusout", formatAxis);
+
 			// Select values in input boxes on focus
 			// more complicated than it looks:
-			//http://stackoverflow.com/questions/3380458/looking-for-a-better-workaround-to-chrome-select-on-focus-bug
+			// http://stackoverflow.com/questions/3380458/looking-for-a-better-workaround-to-chrome-select-on-focus-bug
 			/*
-			jQuery(sphereInput, cylInput, axisInput).on("click", function() {
-				$(this).select();
-			});
-			*/
+			 * jQuery(sphereInput, cylInput, axisInput).on("click", function() {
+			 * $(this).select(); });
+			 */
 
 		});
 	};
-	
+
 	/**
 	 * HTML Input boxes for Rx values
 	 */
 	$.fn.bvd_calculator.inputs = {
-			sphere: null,
-			cyl: null,
-			axis: null
+		sphere : null,
+		cyl : null,
+		axis : null
 	};
-	
+
 	/**
-	 * Defaults 
+	 * Defaults
 	 */
 	$.fn.bvd_calculator.defaults = {
-			sphereSelector: '#sph',
-			cylSelector: '#cyl',
-			axisSelector: '#axis',
-			originalBvdSelector: '#originalBvd',
-			newBvdSelector: '#newBvd',
+		sphereSelector : '#sph',
+		cylSelector : '#cyl',
+		axisSelector : '#axis',
+		originalBvdSelector : '#originalBvd',
+		newBvdSelector : '#newBvd',
 	};
 
 	// Static method.
@@ -72,31 +71,31 @@ window.console.log('Start BVD Calculator');
 	 * Format an input as standard format lens power value
 	 */
 	function formatPower(powerContainer, roundValue) {
-		//default roundValue to false
+		// default roundValue to false
 		roundValue = typeof roundValue !== 'undefined' ? roundValue : false;
-		
-		//get Power value
+
+		// get Power value
 		var powerValue = powerContainer.target.value;
-		
+
 		// Value not set? or PLANO
 		if (powerValue == '' || powerValue == 'PLANO') {
 			powerValue = 0;
 		}
-		
+
 		// Get power as float type
 		var power = parseFloat(powerValue);
-		
-		//round to 0.25??
+
+		// round to 0.25??
 		if (roundValue) {
 			console.log('Rounding...');
-			power = Math.floor(power*4)/4;
+			power = Math.floor(power * 4) / 4;
 		}
-		
+
 		// Round to 2dp
 		power = power.toFixed(2);
-		
-		// Format output	
-		if (power > 0)  {
+
+		// Format output
+		if (power > 0) {
 			finalValue = '+' + power;
 		} else if (power < 0) {
 			finalValue = power;
@@ -104,55 +103,109 @@ window.console.log('Start BVD Calculator');
 			finalValue = 'PLANO';
 		}
 		powerContainer.target.value = finalValue;
-		
+
 		// Update jQuery objects
 		if (powerContainer.target.id == 'sph') {
 			$.fn.bvd_calculator.inputs.sphere = $(powerContainer.target);
 		} else if (powerContainer.target.id == 'cyl') {
 			$.fn.bvd_calculator.inputs.cyl = $(powerContainer.target);
 		}
-		
-		//Validate our Rx
+
+		// Validate our Rx
 		$.fn.bvd_calculator.validate();
-	};
-	
+	}
+	;
+
 	/**
 	 * Format a number as an axis
 	 * 
 	 * Current British Standard specifies no decimal points
 	 */
 	function formatAxis(axisContainer) {
-		//get Axis container
+		// get Axis container
 		var axisValue = axisContainer.target.value;
+
+		// No value set, return no value
 		if (axisValue == '') {
 			return;
 		}
-		
-		// Get axis as float type and round off decimals
+
+		// Get axis value as float type and round off decimals
 		var axis = Math.floor(parseFloat(axisValue));
-		
+
 		// Set input container value
 		axisContainer.target.value = axis;
-	};
-	
+	}
+	;
+
 	/**
 	 * Validate the Rx
 	 */
 	$.fn.bvd_calculator.validate = function() {
-		sphereValue = $.fn.bvd_calculator.inputs.sphere.val();
-		console.log(sphereValue);
-		//if (typeof sphereValue == 'undefined) {}
-		if (this.sphereValue > 40 || this.sphereValue < -40) {
-			sphereInput.addClass('error');
-		}
+		validateSphere();
+		validateCyl();
+		validateAxis();
+
+		console.log($.fn.bvd_calculator.toString());
 	};
-	
+
+	/**
+	 * Validate sphere power is correct
+	 */
+	function validateSphere() {
+		var sphereInput = $.fn.bvd_calculator.inputs.sphere;
+		var sphereValue = parseFloat(sphereInput.val());
+		var sphereParent = sphereInput.parent('label');
+		var sphereError = '';
+
+		// remove old error messages
+		sphereParent.children('small.error').remove();
+
+		// very large power error
+		if (sphereValue > 40 || sphereValue < -40) {
+			sphereError = 'Power out of normal range';
+		}
+
+		// Not multiple of 0.25 error
+		if (sphereValue * 4 != Math.floor(sphereValue * 4)) {
+			sphereError = 'Power not multiple of 0.25';
+		}
+
+		// Add / remove error markers
+		if (sphereError != '') {
+			sphereParent.addClass('error');
+
+			sphereInput.after('<small class="error">' + sphereError
+					+ '</small>');
+		} else {
+			// remove old errors
+			sphereParent.removeClass('error');
+		}
+
+	}
+
+	/**
+	 * Validate cyl power is correct
+	 */
+	function validateCyl() {
+
+	}
+
+	/**
+	 * Validate axis is correct
+	 */
+	function validateAxis() {
+
+	}
 	/**
 	 * Return Rx as String
 	 */
 	$.fn.bvd_calculator.toString = function() {
-		return "To Do!";
-		//return sph + ' / ' + cyl + ' x ' + axis;
+		sph = $.fn.bvd_calculator.inputs.sphere.val();
+		cyl = $.fn.bvd_calculator.inputs.cyl.val();
+		axis = $.fn.bvd_calculator.inputs.axis.val();
+
+		return sph + ' / ' + cyl + ' x ' + axis;
 	};
 
 }(jQuery));
